54a985292c56c98a226c1aff0b663d4349571ebe
g0-0py <brendan@bsherry.io>
2023-07-29 12:45:32 -0500

    commit

--- part1/exercises/src/components/BookList.js
+++ part1/exercises/src/components/BookList.js
@@ -1,15 +1,15 @@
 export default function BookList() {
-   let pageTitle = "shrug";
-   let book1 = "google.com";
-   let book2 = "";
-   let book3 = "";
+   let pageTitle = "books";
+   let book1 = "https://target.scene7.com/is/image/Target/GUEST_c9f49c3e-365e-4aae-a80d-114fb5c496c3?wid=1437&hei=1437&fmt=pjpeg"
+   let book2 = "https://m.media-amazon.com/images/I/91PECfAcJgL.jpg";
+   let book3 = "https://colorem.files.wordpress.com/2016/09/william-yates.jpg?w=761&h=1024";
 
    return (
       <div>
          <h3>{pageTitle}</h3>
-         <img src={book1} alt="Nothing here!" />
-         <img src={book2} alt="Nothing here!" />
-         <img src={book3} alt="Nothing here!" />
+         <img src={book1} alt="Storm Front" />
+         <img src={book2} alt="book about magic" />
+         <img src={book3} alt="a poet" />
       </div>      
    );
 }

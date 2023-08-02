d7e055ad91422ada91d3da97fa382ce930d8b890
g0-0py <brendan@bsherry.io>
2023-08-01 21:50:16 -0500

    part1 studio complete

---
+++ part1/studio/studio/src/components/Description.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import styles from './Description.module.css';
+
+function RecipeAuthor() {
+    const authorLink = "https://www.google.com";
+    const authorPhoto = "placeholder";
+    const authorName = "Menma Bean";
+
+    return (
+        <div className = {styles.recipeAuthorBlock}>
+           <img src={authorPhoto} alt = "A Photo of the Author" className={styles.imageUpdates} />
+           <div>
+              <h3>{authorName}</h3>
+              <a href={authorLink}>Blog name</a> 
+           </div>
+        </div>
+     );
+}
+
+class RecipeDescription extends React.Component {
+    render() {
+        return (
+            <div> 
+                <div>
+                    <h1>Recipe Title</h1>
+                    <p>Short recipe description</p>
+                </div>
+                <RecipeAuthor />
+            </div>
+        )
+    }   
+}
+
+export default RecipeDescription;
---
+++ part1/studio/studio/src/components/Photos.js
@@ -0,0 +1,9 @@
+import styles from './Description.module.css'
+
+function RecipePhoto() {
+    return (
+        <img src="https://rimworldwiki.com/images/thumb/f/f4/Lavish_meal_a.png/250px-Lavish_meal_a.png" alt="recipe photo" className = {styles.imageUpdates} />
+    )
+}
+
+export default RecipePhoto;

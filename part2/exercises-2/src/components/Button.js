1e21479d943bd92545671655a8120c2f298009b2
g0-0py <brendan@bsherry.io>
2023-08-06 14:08:42 -0500

    part2 exercises complete

--- part2/exercises-2/src/components/Button.js
+++ part2/exercises-2/src/components/Button.js
@@ -1,12 +1,18 @@
 import './styles.css';
 
+function onLearnMore() {
+   alert("Oh, hello, do you hear that? The sticky sound, as clear as day. Slip, slop... Drip, drop, slip, slop... How extraordinary. Do you think the water drips, even down deep below at the bottom of the sea? Can you hear it? Drip, drop, slip, slop... *maniacal chuckling* ")
+}
+
 function Button() {
 
    return ( 
-         <button>
+         <button onClick={onLearnMore}>
             Learn More
          </button>
    );
 }
+
+
 
 export default Button;
